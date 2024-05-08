98b820b4660e859df5a1a25d5a2c11938bb9c9d8
Maik <63198269+Maik425@users.noreply.github.com>
2024-05-08 21:18:17 +0900

    feat: もっと見るリンクを追加

--- lmsc/nuxt-app/features/course/courseDetail/components/CourseOverview.vue
+++ lmsc/nuxt-app/features/course/courseDetail/components/CourseOverview.vue
@@ -29,24 +29,57 @@
           <div class="text-left">解説</div>
         </v-col>
         <v-col cols="12" md="10">
-          <div
-            v-for="(paragraph, index) in description"
-            :key="index"
-            class="mb-4"
-          >
-            {{ paragraph }}
+          <div v-if="!showFullDescription">
+            <div
+              v-for="(paragraph, index) in visibleDescription"
+              :key="index"
+              class="mb-4"
+            >
+              {{ paragraph }}
+            </div>
+            <div v-if="description.length > 3" class="text-right">
+              <a
+                href="#"
+                @click.prevent="showFullDescription = true"
+                style="
+                  color: #292737;
+                  text-decoration: underline;
+                  text-underline-offset: 3px;
+                "
+              >
+                もっと見る
+              </a>
+            </div>
+          </div>
+          <div v-else>
+            <div
+              v-for="(paragraph, index) in description"
+              :key="index"
+              class="mb-4"
+            >
+              {{ paragraph }}
+            </div>
           </div>
         </v-col>
       </v-row>
     </v-card-text>
   </v-card>
 </template>
-
 <script setup lang="ts">
+import { ref, computed } from "vue";
+
 const props = defineProps<{
   title: string;
   level: string;
   duration: number;
   description: string[];
 }>();
+
+const showFullDescription = ref(false);
+
+const visibleDescription = computed(() => {
+  return showFullDescription.value
+    ? props.description
+    : props.description.slice(0, 3);
+});
 </script>
