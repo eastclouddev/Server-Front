eb0d8d935da40eef2b50a7eb61a252eb35a4b67c
kotaro-miura <mcotaro070835@gmail.com>
2024-06-04 11:02:42 +0900

    会社情報確認画面　作成

---
+++ lmsc/nuxt-app/pages/company/confirmationAdmin.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <Head>
+        <Title>企業情報 | iDemy</Title>
+        <Meta name="description" content="iDemy | 企業情報確認画面" />
+        </Head>
+        <v-container>
+        <CompanyConfirmationAdmin />
+        </v-container>
+    </div>
+  </template>
+  <script setup>
+  import CompanyConfirmationAdmin from '~/features/CompanyInfomation/CompanyConfirmationAdmin.vue'
+  </script>
