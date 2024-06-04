06af16b95ff7be1220baa1a57ba3492d04517096
Maik <sk8maiki0425@gmail.com>
2024-06-05 01:38:11 +0900

    fix: 軽微な修正

--- lmsc/nuxt-app/features/auth/billing/components/BillingList.vue
+++ lmsc/nuxt-app/features/auth/billing/components/BillingList.vue
@@ -1,8 +1,12 @@
 <template>
   <v-container>
     <div class="d-flex">
-      <img src="/assets/progress.svg" class="pr-5">
-      <v-card-title class="pt-2 pb-2 pl-0 sp_title" style="font-size: 1.6em; font-weight: bold;" color="#242424">
+      <img src="/assets/progress.svg" class="pr-5" />
+      <v-card-title
+        class="pt-2 pb-2 pl-0 sp_title"
+        style="font-size: 1.6em; font-weight: bold"
+        color="#242424"
+      >
         請求履歴一覧
       </v-card-title>
     </div>
@@ -12,10 +16,15 @@
         <v-icon icon="mdi-chevron-right"></v-icon>
       </template>
     </v-breadcrumbs>
-    <v-card flat color="#F5F5F5" style="border-radius: 5px 5px 0 0; box-shadow: 4px 0px 6px -3px rgba(0, 0, 0, 0.2);">
+    <v-card
+      flat
+      color="#F5F5F5"
+      style="
+        border-radius: 5px 5px 0 0;
+        box-shadow: 4px 0px 6px -3px rgba(0, 0, 0, 0.2);
+      "
+    >
       <v-sheet color="#F5F5F5" width="80%" class="mx-auto mb-8 mt-8">
-
-        
         <div class="d-flex justify-space-between">
           <!-- <v-select
             v-model="selectedMonth"
@@ -44,51 +53,54 @@
         </template>
       </v-select>
  -->
- <div class="d-flex">
- <FilterSelect
-            v-model="selectedMonth"
-            :filterOptions="months"
-            placeholderText="請求対象月"
-            @filter-change="filterData"
-          />
-        
-          <FilterSelect
-            v-model="selectedStatus"
-            :filterOptions="statuses"
-            placeholderText="ステータス"
-            @filter-change="filterData"
-          />
+          <div class="d-flex">
+            <FilterSelect
+              v-model="selectedMonth"
+              :filterOptions="months"
+              placeholderText="請求対象月"
+              @filter-change="filterData"
+            />
 
- </div>
+            <FilterSelect
+              v-model="selectedStatus"
+              :filterOptions="statuses"
+              placeholderText="ステータス"
+              @filter-change="filterData"
+            />
+          </div>
 
-      <Button to="/billingInformation"
-        color="#FF5136" class="sp_button" style="width:10rem; height:40px;" buttonText="請求先情報編集">
-      </Button>
-    </div>
-    
-    <template v-if="filteredList.length > 0">
-      <PcBillingTable :filteredList="filteredList" />
-      <SpBillingTable :filteredList="filteredList" />
-    </template>
-    <template v-else>
-      <p class="message">該当するデータがありません</p>
-    </template>
+          <Button
+            to="/billingInformation"
+            color="#FF5136"
+            class="sp_button"
+            style="width: 10rem; height: 40px"
+            buttonText="請求先情報編集"
+          >
+          </Button>
+        </div>
 
+        <template v-if="filteredList.length > 0">
+          <PcBillingTable :filteredList="filteredList" />
+          <SpBillingTable :filteredList="filteredList" />
+        </template>
+        <template v-else>
+          <p class="message">該当するデータがありません</p>
+        </template>
       </v-sheet>
     </v-card>
   </v-container>
 </template>
 
 <script>
-import PcBillingTable from '~/features/auth/billing/components/PcBillingTable.vue';
-import SpBillingTable from '~/features/auth/billing/components/SpBillingTable.vue';
-import FilterSelect from '~/components/FilterSerlect.vue';
+import PcBillingTable from '~/features/auth/billing/components/PcBillingTable.vue'
+import SpBillingTable from '~/features/auth/billing/components/SpBillingTable.vue'
+import FilterSelect from '~/components/FilterSerlect.vue'
 
 export default {
   components: {
     PcBillingTable,
     SpBillingTable,
-    FilterSelect
+    FilterSelect,
   },
   data() {
     return {
@@ -97,11 +109,41 @@ export default {
       months: ['2024/01', '2023/12', '2023/11'],
       statuses: ['未請求', '請求済', '支払済'],
       data: [
-        { id: 1, billingDate: '2024/01', paymentDate: '2024/02/01', payment: 1000, status: '未請求' },
-        { id: 2, billingDate: '2024/01', paymentDate: '2024/02/02', payment: 2000, status: '請求済' },
-        { id: 3, billingDate: '2023/12', paymentDate: '2024/01/01', payment: 1500, status: '支払済' },
-        { id: 4, billingDate: '2023/11', paymentDate: '2023/12/01', payment: 3000, status: '未請求' },
-        { id: 5, billingDate: '2023/11', paymentDate: '2023/12/01', payment: 3000, status: '未請求' }
+        {
+          id: 1,
+          billingDate: '2024/01',
+          paymentDate: '2024/02/01',
+          payment: 1000,
+          status: '未請求',
+        },
+        {
+          id: 2,
+          billingDate: '2024/01',
+          paymentDate: '2024/02/02',
+          payment: 2000,
+          status: '請求済',
+        },
+        {
+          id: 3,
+          billingDate: '2023/12',
+          paymentDate: '2024/01/01',
+          payment: 1500,
+          status: '支払済',
+        },
+        {
+          id: 4,
+          billingDate: '2023/11',
+          paymentDate: '2023/12/01',
+          payment: 3000,
+          status: '未請求',
+        },
+        {
+          id: 5,
+          billingDate: '2023/11',
+          paymentDate: '2023/12/01',
+          payment: 3000,
+          status: '未請求',
+        },
       ],
       items: [
         {
@@ -114,7 +156,7 @@ export default {
           disabled: false,
         },
       ],
-    };
+    }
   },
   computed: {
     filteredList() {
@@ -122,21 +164,20 @@ export default {
         return (
           (!this.selectedMonth || item.billingDate === this.selectedMonth) &&
           (!this.selectedStatus || item.status === this.selectedStatus)
-        );
-      });
-    }
+        )
+      })
+    },
   },
   methods: {
-    filterData() {
-    }
-  }
-};
+    filterData() {},
+  },
+}
 </script>
 
 <style>
 .unclaimed {
-  color: #FFFFFF;
-  background-color: #FF5A36;
+  color: #ffffff;
+  background-color: #ff5a36;
   font-weight: bold;
   padding: 3px;
   width: 6rem;
@@ -145,8 +186,8 @@ export default {
   margin-left: 10px;
 }
 .paid {
-  color: #FFFFFF;
-  background-color: #8A4E41;
+  color: #ffffff;
+  background-color: #8a4e41;
   font-weight: bold;
   padding: 3px;
   width: 6rem;
@@ -155,7 +196,7 @@ export default {
   margin-left: 10px;
 }
 .billed {
-  color: #FFFFFF;
+  color: #ffffff;
   background-color: #292737;
   font-weight: bold;
   padding: 3px;
@@ -172,6 +213,6 @@ export default {
 }
 
 .v-select .v-field.v-field {
-  background-color: #FFFFFF;
+  background-color: #ffffff;
 }
 </style>
--- lmsc/nuxt-app/features/auth/billing/components/BillingModal.vue
+++ lmsc/nuxt-app/features/auth/billing/components/BillingModal.vue
@@ -1,46 +1,76 @@
 <template>
   <v-dialog max-width="1040px" height="auto">
     <template v-slot:activator="{ props: activatorProps }">
-      <v-card flat class="text-center sp_text" style="background-color: transparent;">
-        <a v-bind="activatorProps" style="cursor: pointer; text-decoration: underline; color:#FF5136;">詳細</a>
+      <v-card
+        flat
+        class="text-center sp_text"
+        style="background-color: transparent"
+      >
+        <a
+          v-bind="activatorProps"
+          style="cursor: pointer; text-decoration: underline; color: #ff5136"
+          >詳細</a
+        >
       </v-card>
     </template>
     <template v-slot:default>
       <v-card-actions>
         <v-spacer></v-spacer>
-        <Button color="#FFFFFF" style="background-color: #FF5136; width:10rem;"
-        buttonText="ダウンロード"
-        icon="mdi-download"
-        @click="downloadInvoice">
+        <Button
+          color="#FFFFFF"
+          style="background-color: #ff5136; width: 10rem"
+          buttonText="ダウンロード"
+          icon="mdi-download"
+          @click="downloadInvoice"
+        >
         </Button>
       </v-card-actions>
       <v-card class="pa-5 ma-5">
         <v-card-text>
           <v-sheet class="d-flex justify-space-between">
             <div>
-              <h1 style="color:#FF5A36;">請求書</h1>
-              <div class="pa-2 d-flex justify-space-between" style="font-weight:bold;">
-                <span>株式会社〇〇</span><span style="color:#FF5A36;">御中</span>
+              <h1 style="color: #ff5a36">請求書</h1>
+              <div
+                class="pa-2 d-flex justify-space-between"
+                style="font-weight: bold"
+              >
+                <span>株式会社〇〇</span
+                ><span style="color: #ff5a36">御中</span>
               </div>
-              <v-divider  style="color:#FF5A36; opacity:1;"></v-divider>
-              <p class="pa-2" style="color:#FF5A36; font-size:0.8em;">下記の通りにご請求申しあげます。</p>
+              <v-divider style="color: #ff5a36; opacity: 1"></v-divider>
+              <p class="pa-2" style="color: #ff5a36; font-size: 0.8em">
+                下記の通りにご請求申しあげます。
+              </p>
             </div>
             <p>2024年3月1日</p>
           </v-sheet>
           <v-sheet class="mb-10 d-flex justify-space-between align-end">
             <div>
-              <div  class="d-flex">
-                <p class="d-flex align-center justify-center pa-1 mb-2 mr-10"
-                  style="color:#FFFFFF; background-color: #FF5136; width:8rem; border-radius: 5px; font-weight:bold;">
-                ご請求金額
+              <div class="d-flex">
+                <p
+                  class="d-flex align-center justify-center pa-1 mb-2 mr-10"
+                  style="
+                    color: #ffffff;
+                    background-color: #ff5136;
+                    width: 8rem;
+                    border-radius: 5px;
+                    font-weight: bold;
+                  "
+                >
+                  ご請求金額
                 </p>
-                <p style="font-size:2em; font-weight:bold;">￥０００００００-</p>
+                <p style="font-size: 2em; font-weight: bold">
+                  ￥０００００００-
+                </p>
               </div>
-              <v-divider  style="color:#FF5A36; opacity:1;" height="10px"></v-divider>
+              <v-divider
+                style="color: #ff5a36; opacity: 1"
+                height="10px"
+              ></v-divider>
             </div>
             <div class="d-flex flex-column">
               <div class="d-flex align-end">
-                <p style="font-weight:bold;" class="mr-10">株式会社〇〇</p>
+                <p style="font-weight: bold" class="mr-10">株式会社〇〇</p>
                 <span class="stanp">印</span>
               </div>
               <div>
@@ -49,97 +79,190 @@
                 <p>電話番号</p>
                 <p>メールアドレス</p>
               </div>
-
             </div>
           </v-sheet>
 
           <v-table density="compact" width="100%">
-            <thead style="color: #FFFFFF; background-color: #FF5A36;">
+            <thead style="color: #ffffff; background-color: #ff5a36">
               <tr>
-                <th class="text-center pa-0" style="font-weight: bold; border: 2px solid #FFFFFF; border-radius: 5px;">No.</th>
-                <th class="text-center pa-0" style="font-weight: bold; border: 2px solid #FFFFFF; border-radius: 5px;">内訳</th>
-                <th class="text-center pa-0" style="font-weight: bold; border: 2px solid #FFFFFF; border-radius: 5px;">数量</th>
-                <th class="text-center pa-0" style="font-weight: bold; border: 2px solid #FFFFFF; border-radius: 5px;">単価</th>
-                <th class="text-center pa-0" style="font-weight: bold; border: 2px solid #FFFFFF; border-radius: 5px;">合計</th>
+                <th
+                  class="text-center pa-0"
+                  style="
+                    font-weight: bold;
+                    border: 2px solid #ffffff;
+                    border-radius: 5px;
+                  "
+                >
+                  No.
+                </th>
+                <th
+                  class="text-center pa-0"
+                  style="
+                    font-weight: bold;
+                    border: 2px solid #ffffff;
+                    border-radius: 5px;
+                  "
+                >
+                  内訳
+                </th>
+                <th
+                  class="text-center pa-0"
+                  style="
+                    font-weight: bold;
+                    border: 2px solid #ffffff;
+                    border-radius: 5px;
+                  "
+                >
+                  数量
+                </th>
+                <th
+                  class="text-center pa-0"
+                  style="
+                    font-weight: bold;
+                    border: 2px solid #ffffff;
+                    border-radius: 5px;
+                  "
+                >
+                  単価
+                </th>
+                <th
+                  class="text-center pa-0"
+                  style="
+                    font-weight: bold;
+                    border: 2px solid #ffffff;
+                    border-radius: 5px;
+                  "
+                >
+                  合計
+                </th>
               </tr>
             </thead>
             <tbody>
-              <tr v-for="(item, index) in items" :key="item.id"
-                :style="{ backgroundColor: index % 2 === 0 ? '#FFFFFF' : '#FFFAF4'}">
+              <tr
+                v-for="(item, index) in items"
+                :key="item.id"
+                :style="{
+                  backgroundColor: index % 2 === 0 ? '#FFFFFF' : '#FFFAF4',
+                }"
+              >
                 <td
                   :style="{
-                  borderRight: '2px solid #FF5A36',
-                  borderLeft: '2px solid #FF5A36',
-                  borderTop: index === 0 ? '2px solid #FF5A36' : 'none',
-                  borderBottom: index === items.length - 1 ? '2px solid #FF5A36' : 'none',
-                  borderTopLeftRadius: index === 0 ? '5px' : '0',
-                  borderBottomLeftRadius: index === items.length - 1 ? '5px' : '0',
-                  width: '30px'
+                    borderRight: '2px solid #FF5A36',
+                    borderLeft: '2px solid #FF5A36',
+                    borderTop: index === 0 ? '2px solid #FF5A36' : 'none',
+                    borderBottom:
+                      index === items.length - 1 ? '2px solid #FF5A36' : 'none',
+                    borderTopLeftRadius: index === 0 ? '5px' : '0',
+                    borderBottomLeftRadius:
+                      index === items.length - 1 ? '5px' : '0',
+                    width: '30px',
                   }"
-                  class="text-center">
+                  class="text-center"
+                >
                   {{ index + 1 }}
                 </td>
                 <td
                   :style="{
-                  borderRight: '2px solid #FF5A36',
-                  borderTop: index === 0 ? '2px solid #FF5A36' : 'none',
-                  borderBottom: index === items.length - 1 ? '2px solid #FF5A36' : 'none',
-                  }">
+                    borderRight: '2px solid #FF5A36',
+                    borderTop: index === 0 ? '2px solid #FF5A36' : 'none',
+                    borderBottom:
+                      index === items.length - 1 ? '2px solid #FF5A36' : 'none',
+                  }"
+                >
                   {{ item.description }}
                 </td>
                 <td
                   :style="{
-                  borderRight: '2px solid #FF5A36',
-                  borderTop: index === 0 ? '2px solid #FF5A36' : 'none',
-                  borderBottom: index === items.length - 1 ? '2px solid #FF5A36' : 'none',
-                  width: '30px'
+                    borderRight: '2px solid #FF5A36',
+                    borderTop: index === 0 ? '2px solid #FF5A36' : 'none',
+                    borderBottom:
+                      index === items.length - 1 ? '2px solid #FF5A36' : 'none',
+                    width: '30px',
                   }"
-                  class="text-center">
+                  class="text-center"
+                >
                   {{ item.order }}
                 </td>
                 <td
                   :style="{
-                  borderRight: '2px solid #FF5A36',
-                  borderTop: index === 0 ? '2px solid #FF5A36' : 'none',
-                  borderBottom: index === items.length - 1 ? '2px solid #FF5A36' : 'none',
-                  width: '100px'
+                    borderRight: '2px solid #FF5A36',
+                    borderTop: index === 0 ? '2px solid #FF5A36' : 'none',
+                    borderBottom:
+                      index === items.length - 1 ? '2px solid #FF5A36' : 'none',
+                    width: '100px',
                   }"
-                  class="text-center">
+                  class="text-center"
+                >
                   {{ item.amount }}
                 </td>
                 <td
                   :style="{
-                  borderRight: '2px solid #FF5A36',
-                  borderTop: index === 0 ? '2px solid #FF5A36' : 'none',
-                  borderBottom: index === items.length - 1 ? '2px solid #FF5A36' : 'none',
-                  borderTopRightRadius: index === 0 ? '5px' : '0',
-                  width: '100px'
+                    borderRight: '2px solid #FF5A36',
+                    borderTop: index === 0 ? '2px solid #FF5A36' : 'none',
+                    borderBottom:
+                      index === items.length - 1 ? '2px solid #FF5A36' : 'none',
+                    borderTopRightRadius: index === 0 ? '5px' : '0',
+                    width: '100px',
                   }"
-                  class="text-center">
-                   <span v-if="item.order * item.amount">{{ item.order * item.amount }}</span>
+                  class="text-center"
+                >
+                  <span v-if="item.order * item.amount">{{
+                    item.order * item.amount
+                  }}</span>
                 </td>
               </tr>
             </tbody>
           </v-table>
           <div class="d-flex justify-space-between">
             <div>
-              <h4 style="color:#FF5A36;" class="mt-3">備考欄</h4>
+              <h4 style="color: #ff5a36" class="mt-3">備考欄</h4>
               <v-text-field
                 width="600px"
                 variant="plain"
                 disabled
-                style="border: 1px solid #FF5A36; border-radius: 5px; height: 75px;">
+                style="
+                  border: 1px solid #ff5a36;
+                  border-radius: 5px;
+                  height: 75px;
+                "
+              >
               </v-text-field>
             </div>
-            <v-table class="mb-5" style="width:244px; border: 2px solid #FF5A36; border-top:none; border-radius:0 0 5px 5px;" >
+            <v-table
+              class="mb-5"
+              style="
+                width: 244px;
+                border: 2px solid #ff5a36;
+                border-top: none;
+                border-radius: 0 0 5px 5px;
+              "
+            >
               <tbody>
-                <tr v-for="(row, index) in summaryRows" :key="index" :style="{ backgroundColor: index % 2 === 0 ? '#FFFFFF' : '#FFFAF4'}"
-                  class="d-flex ">
-                  <th class="flex-grow-1 text-center pa-2"
-                  style="color: #FF5136; ;border-right: 2px solid #FF5A36;width: 142px; height: 36px; font-weight:bold; font-size:0.9em;">
+                <tr
+                  v-for="(row, index) in summaryRows"
+                  :key="index"
+                  :style="{
+                    backgroundColor: index % 2 === 0 ? '#FFFFFF' : '#FFFAF4',
+                  }"
+                  class="d-flex"
+                >
+                  <th
+                    class="flex-grow-1 text-center pa-2"
+                    style="
+                      color: #ff5136;
+                      border-right: 2px solid #ff5a36;
+                      width: 142px;
+                      height: 36px;
+                      font-weight: bold;
+                      font-size: 0.9em;
+                    "
+                  >
                     {{ row.label }}
                   </th>
-                  <td class="flex-grow-1 text-center pa-2" style="width: 98px; height: 36px;">
+                  <td
+                    class="flex-grow-1 text-center pa-2"
+                    style="width: 98px; height: 36px"
+                  >
                     ¥{{ row.value }}
                   </td>
                 </tr>
@@ -148,18 +271,23 @@
           </div>
 
           <v-sheet>
-            <h4 class="text-center pt-1 pb-1 mb-3" style="color:#FFFFFF; background-color: #FF5136; width:8rem; border-radius: 5px; font-weight:bold;">
+            <h4
+              class="text-center pt-1 pb-1 mb-3"
+              style="
+                color: #ffffff;
+                background-color: #ff5136;
+                width: 8rem;
+                border-radius: 5px;
+                font-weight: bold;
+              "
+            >
               お振込先情報
             </h4>
-            <v-card flat class="pa-3" style="border: 1px solid #FF5A36;">
+            <v-card flat class="pa-3" style="border: 1px solid #ff5a36">
               <p>振り込み期日:</p>
               <p>銀行情報</p>
             </v-card>
           </v-sheet>
-          
-
-
-          
         </v-card-text>
       </v-card>
     </template>
@@ -231,53 +359,46 @@ export default {
           order: '',
           amount: '',
         },
-      ]
-    };
+      ],
+    }
   },
-    computed: {
+  computed: {
     subtotal() {
-      return this.items.reduce((sum, item) => sum + (item.order * item.amount), 0);
+      return this.items.reduce((sum, item) => sum + item.order * item.amount, 0)
     },
     tax() {
-      return Math.round(this.subtotal * 0.1); // 消費税10%と仮定
+      return Math.round(this.subtotal * 0.1) // 消費税10%と仮定
     },
     total() {
-      return this.subtotal + this.tax;
+      return this.subtotal + this.tax
     },
     summaryRows() {
       return [
         { label: '小計', value: this.subtotal },
         { label: '消費税', value: this.tax },
-        { label: '合計金額', value: this.total }
-      ];
-    }
+        { label: '合計金額', value: this.total },
+      ]
+    },
   },
   methods: {
     downloadInvoice() {
       // ダウンロードロジック追加
-      alert("請求書をダウンロードします。");
-    }
-  }
-};
+      alert('請求書をダウンロードします。')
+    },
+  },
+}
 </script>
 <style lang="scss" scoped>
 .stanp {
   width: 80px;
   height: 80px;
   line-height: 80px;
-  border: 2px solid #CC092B;
-  color: #CC092B;
+  border: 2px solid #cc092b;
+  color: #cc092b;
   border-radius: 50%;
-  background-color: #FFFFFF;
+  background-color: #ffffff;
   text-align: center;
   display: inline-block;
   margin-bottom: 10px;
-}
-@media screen and (max-width: 768px) {
-  .sp {
-    &_text {
-      font-size: 1.4em;
-    }
-  }
 }
 </style>
