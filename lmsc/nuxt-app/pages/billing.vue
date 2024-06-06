868c189564e626ed3757dcf8efd14fe491c509b4
kotaro-miura <mcotaro070835@gmail.com>
2024-06-06 17:12:01 +0900

    develop branch merge

---
+++ lmsc/nuxt-app/pages/billing.vue
@@ -0,0 +1,12 @@
+<template>
+  <Head>
+    <Title>請求履歴 | iDemy</Title>
+    <Meta name="description" content="iDemy | 請求履歴一覧画面" />
+  </Head>
+  <v-container>
+    <BillingList />
+  </v-container>
+</template>
+<script setup>
+import BillingList from '~/features/auth/billing/components/BillingList.vue';
+</script>
