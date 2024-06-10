0a29a746ca827c5a41de02a7c2188833798e5b4b
natsukimizumoto <107124212+natsukimizumoto@users.noreply.github.com>
2024-06-10 21:30:40 +0900

    ダッシュボード修正

--- lmsc/nuxt-app/features/auth/dashboard/components/DashboardStudent.vue
+++ lmsc/nuxt-app/features/auth/dashboard/components/DashboardStudent.vue
@@ -4,15 +4,15 @@
     <!-- イニシャルでの実装なし -->
     <!-- <StudyItem /> -->
     <ProgressItem />
-    <CouseItem />
+    <CourseItem />
     <RecommendationItem />
   </div>
 </template>
 <script>
-import NewsItem from "~/features/auth/dashboard/components/NewsItem.vue";
+import NewsItem from "~/features/auth/dashboard/components/newsnews.vue";
 import StudyItem  from "~/features/auth/dashboard/components/StudyItem.vue";
 import ProgressItem  from "~/features/auth/dashboard/components/ProgressItem.vue";
-import CouseItem  from "~/features/auth/dashboard/components/CouseItem.vue";
+import CourseItem  from "~/features/auth/dashboard/components/CourseItem.vue";
 import RecommendationItem from "~/features/auth/dashboard/components/RecommendationItem.vue";
 
 export default {
@@ -20,7 +20,7 @@ export default {
     NewsItem,
     StudyItem,
     ProgressItem,
-    CouseItem,
+    CourseItem,
     RecommendationItem
   },
 }
