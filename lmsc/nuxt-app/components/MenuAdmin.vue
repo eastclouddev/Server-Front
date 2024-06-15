b549ed53f1ae7447e79331e4875723372c176211
Maik <sk8maiki0425@gmail.com>
2024-06-16 01:37:15 +0900

    feat: ログアウト時にloginページに遷移するように修正

--- lmsc/nuxt-app/components/MenuAdmin.vue
+++ lmsc/nuxt-app/components/MenuAdmin.vue
@@ -1,12 +1,29 @@
 <template>
   <v-list>
-    <v-sheet v-for="(section, index) in settingsSections" :key="index" class="pa-3">
+    <v-sheet
+      v-for="(section, index) in settingsSections"
+      :key="index"
+      class="pa-3"
+    >
       <v-list-item v-if="section.icon && section.title" class="pb-5">
         <div class="d-flex align-center">
-          <img :src="section.icon" alt="icon" class="pr-3" style="width:40px;">
-          <v-list-item-title style="font-weight: bold;">{{ section.title }}</v-list-item-title>
+          <img
+            :src="section.icon"
+            alt="icon"
+            class="pr-3"
+            style="width: 40px"
+          />
+          <v-list-item-title style="font-weight: bold">{{
+            section.title
+          }}</v-list-item-title>
         </div>
-        <NuxtLink v-for="(link, linkIndex) in section.links" :to="link.url" class="d-flex align-start pt-3" :key="linkIndex" style="text-decoration: none; color:#242424;">
+        <NuxtLink
+          v-for="(link, linkIndex) in section.links"
+          :to="link.url"
+          class="d-flex align-start pt-3"
+          :key="linkIndex"
+          style="text-decoration: none; color: #242424"
+        >
           {{ link.text }}
         </NuxtLink>
       </v-list-item>
@@ -18,8 +35,8 @@
 </template>
 
 <script>
-import StudyIcon from '~/assets/study.svg';
-import SettingIcon from '~/assets/setting.svg';
+import StudyIcon from '~/assets/study.svg'
+import SettingIcon from '~/assets/setting.svg'
 export default {
   data() {
     return {
@@ -33,7 +50,7 @@ export default {
             { text: 'コース一覧', url: '/' },
             { text: 'ユーザー一覧', url: '/' },
             { text: 'コース作成', url: '/' },
-          ]
+          ],
         },
         {
           icon: SettingIcon,
@@ -42,10 +59,10 @@ export default {
             { text: 'パスワード変更', url: '/' },
             { text: 'メールアドレス変更', url: '/' },
             { text: '企業情報', url: '/' },
-            { text: 'ログアウト', url: '/' },
-          ]
-        }
-      ]
+            { text: 'ログアウト', url: '/login' },
+          ],
+        },
+      ],
     }
   },
 }
--- lmsc/nuxt-app/components/MenuStudent.vue
+++ lmsc/nuxt-app/components/MenuStudent.vue
@@ -1,24 +1,41 @@
 <template>
-    <v-list>
-      <v-sheet v-for="(section, index) in settingsSections" :key="index" class="pa-3">
-        <v-list-item v-if="section.icon && section.title" class="pb-5">
-          <div class="d-flex">
-            <img :src="section.icon" alt="icon" class="pr-3" style="width: 35px;">
-            <v-list-item-title style="font-weight:bold;">{{ section.title }}</v-list-item-title>
-          </div>
-          <NuxtLink v-for="(link, linkIndex) in section.links" :to="link.url" class="d-flex align-start pt-3" :key="linkIndex" style="text-decoration: none; color:#242424;">
+  <v-list>
+    <v-sheet
+      v-for="(section, index) in settingsSections"
+      :key="index"
+      class="pa-3"
+    >
+      <v-list-item v-if="section.icon && section.title" class="pb-5">
+        <div class="d-flex">
+          <img
+            :src="section.icon"
+            alt="icon"
+            class="pr-3"
+            style="width: 35px"
+          />
+          <v-list-item-title style="font-weight: bold">{{
+            section.title
+          }}</v-list-item-title>
+        </div>
+        <NuxtLink
+          v-for="(link, linkIndex) in section.links"
+          :to="link.url"
+          class="d-flex align-start pt-3"
+          :key="linkIndex"
+          style="text-decoration: none; color: #242424"
+        >
           {{ link.text }}
-          </NuxtLink>
-        </v-list-item>
-        <template v-if="index !== settingsSections.length - 1">
-          <v-divider color="#BFBFBF"></v-divider>
-        </template>
-      </v-sheet>
-    </v-list>
+        </NuxtLink>
+      </v-list-item>
+      <template v-if="index !== settingsSections.length - 1">
+        <v-divider color="#BFBFBF"></v-divider>
+      </template>
+    </v-sheet>
+  </v-list>
 </template>
 <script>
-import StudyIcon from '~/assets/study.svg';
-import SettingIcon from '~/assets/setting.svg';
+import StudyIcon from '~/assets/study.svg'
+import SettingIcon from '~/assets/setting.svg'
 export default {
   data() {
     return {
@@ -29,18 +46,18 @@ export default {
           links: [
             { text: 'コース一覧', url: '/' },
             { text: '質問リスト', url: '/' },
-            { text: '課題提出', url: '/' }
-          ]
+            { text: '課題提出', url: '/' },
+          ],
         },
         {
           icon: SettingIcon,
           title: '設定',
           links: [
             { text: 'パスワード変更', url: '/' },
-            { text: 'ログアウト', url: '/' }
-          ]
-        }
-      ]
+            { text: 'ログアウト', url: '/login' },
+          ],
+        },
+      ],
     }
   },
 }
