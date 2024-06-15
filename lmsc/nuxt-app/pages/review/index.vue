2138a0e49e9821bb48cd91afdc42f1d26bb5c87a
Maik <sk8maiki0425@gmail.com>
2024-06-16 02:12:51 +0900

    feat: ユーザのロールごとに別のAPIを呼び出す

--- lmsc/nuxt-app/pages/review/index.vue
+++ lmsc/nuxt-app/pages/review/index.vue
@@ -3,39 +3,57 @@
     <Title>レビュー一覧 | iDemy</Title>
     <Meta name="description" content="iDemy | レビュー一覧画面" />
   </Head>
-  <!-- <LoadingAndError :error="myReviewListError" :status="myReviewListStatus"> -->
-  <v-container>
-    <ReviewSummary :reviews="reviews" />
-    <div v-if="myReviewListStatus === 'pending'">Loading...</div>
-    <div v-else-if="myReviewListError">
-      Error: {{ myReviewListError.message }}
-    </div>
-  </v-container>
-  <!-- </LoadingAndError> -->
+  <LoadingAndError :error="reviewListError" :status="reviewListStatus">
+    <v-container>
+      <ReviewSummary :reviews="reviews" />
+      <div v-if="reviewListStatus === 'pending'">Loading...</div>
+      <div v-else-if="reviewListError">
+        Error: {{ reviewListError.message }}
+      </div>
+    </v-container>
+  </LoadingAndError>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue'
 import ReviewSummary from '~/features/review/components/ReviewSummary.vue'
-import { useGetMyReviewList } from '~/features/review/api/getMyReviewList'
-// import LoadingAndError from '~/components/LoadingAndError.vue'
+import { fetchReviewList } from '~/features/review/api/fetchReviewList'
+import { useUserStore } from '~/store/user'
+import LoadingAndError from '~/components/LoadingAndError.vue'
 
-const studentId = 3
-const {
-  data: myReviewListData,
-  error: myReviewListError,
-  status: myReviewListStatus,
-} = useGetMyReviewList(studentId)
+const userStore = useUserStore()
+const userId = userStore.$state.user?.id
+const userRole = userStore.$state.user?.role_id
+
 const reviews = ref([])
+const reviewListError = ref(null)
+const reviewListStatus = ref('pending')
 
-if (myReviewListData.value) {
-  reviews.value = myReviewListData.value.reviews
+const fetchReviews = async () => {
+  if (!userId || userRole === undefined) {
+    console.log('User ID or role is missing')
+    reviewListError.value = new Error('User ID or role is missing')
+    reviewListStatus.value = 'error'
+    return
+  }
+
+  console.log('Fetching reviews...')
+  reviewListStatus.value = 'pending'
+  const { data, error, status } = await fetchReviewList(userId, userRole)
+  reviewListError.value = error.value
+  reviewListStatus.value = status.value
+
+  if (data.value) {
+    reviews.value = data.value.reviews
+  } else {
+    console.log('No data fetched')
+  }
 }
 
-console.log(
-  'myReviewListData',
-  myReviewListData.value ? myReviewListData.value.reviews : []
-)
-console.log('myReviewListError', myReviewListError)
-console.log('myReviewListStatus', myReviewListStatus)
+onMounted(() => {
+  console.log('Component mounted')
+  console.log('userId:', userId)
+  console.log('userRole:', userRole)
+  fetchReviews()
+})
 </script>
