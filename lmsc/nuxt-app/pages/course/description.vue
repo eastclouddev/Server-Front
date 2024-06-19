5abfa6eeae44b908c0028af25a1b98d2fc6b0c73
natsukimizumoto <107124212+natsukimizumoto@users.noreply.github.com>
2024-06-19 17:46:50 +0900

    コース詳細UI作成

---
+++ lmsc/nuxt-app/pages/course/description.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Head>
+      <Title>コース詳細画面 | iDemy</Title>
+      <Meta name="description" content="iDemy | コース詳細画面" />
+    </Head>
+    <v-container>
+      <CourseDescription />
+    </v-container>
+  </div>
+</template>
+
+  <script setup>
+  import CourseDescription from '~/features/course/courseDetail/components/CourseDescription.vue'
+  </script>
