74159c0250e0ba6f9f8df182fd5f48004bde3d88
natsukimizumoto <107124212+natsukimizumoto@users.noreply.github.com>
2024-07-01 19:05:14 +0900

    コンフリクト解消

--- lmsc/nuxt-app/components/MenuCorporation.vue
+++ lmsc/nuxt-app/components/MenuCorporation.vue
@@ -1,5 +1,6 @@
 <template>
   <v-list>
+<<<<<<< HEAD
     <v-sheet
       v-for="(section, index) in settingsSections"
       :key="index"
@@ -24,6 +25,15 @@
           style="text-decoration: none; color: #242424"
           @click="link.text === 'ログアウト' ? handleLogout($event) : null"
         >
+=======
+    <v-sheet v-for="(section, index) in settingsSections" :key="index" class="pa-3">
+      <v-list-item v-if="section.icon && section.title" class="pb-5">
+        <div class="d-flex align-center">
+          <v-img :src="section.icon" class="mr-3" height="24" width="24"></v-img>
+          <v-list-item-title style="font-weight: bold;">{{ section.title }}</v-list-item-title>
+        </div>
+        <NuxtLink v-for="(link, linkIndex) in section.links" :to="link.url" class="d-flex align-start pt-3" :key="linkIndex" style="text-decoration: none; color:#242424;">
+>>>>>>> c0313ab (UI修正中)
           {{ link.text }}
         </NuxtLink>
       </v-list-item>
@@ -50,7 +60,11 @@ export default defineComponent({
       settingsSections: [
         {
           title: '進捗管理',
+<<<<<<< HEAD
           icon: StudyIcon,
+=======
+          icon: '',
+>>>>>>> c0313ab (UI修正中)
           links: [
             { text: '進捗管理一覧', url: '/' },
             { text: 'コース一覧', url: '/' },
@@ -58,8 +72,12 @@ export default defineComponent({
           ],
         },
         {
+<<<<<<< HEAD
           icon: SettingIcon,
+=======
+>>>>>>> c0313ab (UI修正中)
           title: '設定',
+          icon: '',
           links: [
             { text: 'パスワード変更', url: '/' },
             { text: 'メールアドレス変更', url: '/' },
@@ -71,5 +89,14 @@ export default defineComponent({
       ],
     }
   },
+<<<<<<< HEAD
 })
 </script>
+=======
+  mounted() {
+    this.settingsSections[0].icon = 'assets/progress.svg';
+    this.settingsSections[1].icon = 'assets/setting.svg';
+  }
+}
+</script>
+>>>>>>> c0313ab (UI修正中)
