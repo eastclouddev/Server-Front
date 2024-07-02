086bbff2bb442efb1a167c8bf484846a83ba4e75
natsukimizumoto <107124212+natsukimizumoto@users.noreply.github.com>
2024-07-02 13:55:20 +0900

    ディレクトリ作成

---
+++ lmsc/nuxt-app/features/question/api/getQuestion.ts
@@ -0,0 +1,36 @@
+import { useAsyncData } from '#app'
+import apiClient from '@/libs/axios'
+import { AxiosError } from 'axios'
+import { service } from '@/constants/service'
+
+export async function getReviewRequest(reviewRequestId: number) {
+  try {
+    const response = await apiClient.get(`/reviews/${reviewRequestId}`)
+
+    console.log('API取得データ:', response.data)
+    return response.data
+  } catch (err) {
+    const axiosError = err as AxiosError
+    if (axiosError.response) {
+      console.error('レスポンスエラー:', axiosError.response)
+    } else if (axiosError.request) {
+      console.error('リクエストエラー:', axiosError.request)
+    } else {
+      console.error('設定エラー:', axiosError.message)
+    }
+    if (axiosError.response?.status !== 404) {
+      throw new Error(service.apiFetchError)
+    }
+    throw err
+  }
+}
+
+export function useGetReviewRequest(reviewRequestId: number) {
+  const { data, error, status } = useAsyncData(
+    `reviewRequest-${reviewRequestId}`,
+    () => getReviewRequest(reviewRequestId),
+    { server: true }
+  )
+
+  return { data, error, status }
+}
